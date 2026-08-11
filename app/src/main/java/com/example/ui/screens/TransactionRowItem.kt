253b304a5e39e37f1cc6b0d605d0058adb76b2da
smaks190-crypto <smaks190@gmail.com>
2026-08-11 15:44:21 +0300

    Add import for getCategoryColorAndIcon function

--- app/src/main/java/com/example/ui/screens/TransactionRowItem.kt
+++ app/src/main/java/com/example/ui/screens/TransactionRowItem.kt
@@ -1,5 +1,6 @@
 package com.example.ui.screens
 
+import com.example.ui.screens.getCategoryColorAndIcon
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
