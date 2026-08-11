ad885ea76d587ef7910bee4adb536bba42c817aa
smaks190-crypto <smaks190@gmail.com>
2026-08-11 15:49:42 +0300

    Update PeriodBudgetScreen.kt

--- app/src/main/java/com/example/ui/screens/PeriodBudgetScreen.kt
+++ app/src/main/java/com/example/ui/screens/PeriodBudgetScreen.kt
@@ -1,6 +1,5 @@
 package com.example.ui.screens
 
-import com.example.ui.components.VoiceInputNeonCapsule
 import android.widget.Toast
 import com.example.ui.utils.MonthsRu
 import androidx.compose.animation.AnimatedVisibility
