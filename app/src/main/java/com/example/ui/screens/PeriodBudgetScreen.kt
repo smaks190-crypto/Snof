c2a120654d3b2204edd2f905922c6987cd588857
smaks190-crypto <smaks190@gmail.com>
2026-08-11 14:19:41 +0300

    Update PeriodBudgetScreen.kt

--- app/src/main/java/com/example/ui/screens/PeriodBudgetScreen.kt
+++ app/src/main/java/com/example/ui/screens/PeriodBudgetScreen.kt
@@ -349,7 +349,6 @@ fun PeriodBudgetScreen(
                 val currentPeriodInitialDate = if (periodType == PeriodType.DAY || periodType == PeriodType.WEEK) selectedDateDay else monthStart
                 AllTransactionsDialog(
                     transactions = if (allTransactions.isNotEmpty()) allTransactions.filter { it.parentId.isNullOrBlank() } else mainFilteredTransactions,
-                    allTransactions = allTransactions,
                     onDeleteTransaction = onDeleteTransaction,
                     onEditTransaction = onEditTransaction,
                     initialFilterType = initialAllTransactionsFilter,
